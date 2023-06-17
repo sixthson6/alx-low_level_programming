fa163dab98067fca517c486c68175371d8dfb3af
sixthson6 <sudais2408@gmail.com>
2023-06-16 20:12:24 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
- *
+/**
  * main - use printf
  *
- * Return 0;
+ * Return: 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
