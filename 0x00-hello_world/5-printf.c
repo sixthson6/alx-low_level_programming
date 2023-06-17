88ff283fe056ac5eef816c841707b610b924b987
sixthson6 <sudais2408@gmail.com>
2023-06-16 19:30:22 -0700

    printff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  * Return 0;
  */
 int main() {
-	printf("with proper grammar, but the outcome is a piece of art");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 } 
