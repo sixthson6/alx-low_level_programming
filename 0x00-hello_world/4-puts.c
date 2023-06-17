4b5ec6e83492b119d6099151410374768281a910
sixthson6 <sudais2408@gmail.com>
2023-06-16 18:41:06 -0700

    my first C coodre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* go to display using puts */
 int main()
 {
-	const char *message = "\"Programming is like building a multilingual puzzle\n";
+	const char *message = "\"Programming is like building a multilingual puzzle";
 	puts(message);
 	return 0;
 }
