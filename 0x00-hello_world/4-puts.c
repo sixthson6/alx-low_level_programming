d28a5d4c9f348bebda99cccee2ae691420b5fb96
sixthson6 <sudais2408@gmail.com>
2023-06-16 19:45:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * Return: always 0
  */
-int main(void) 
+int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
-	puts(message); 
+	puts(message);
 	return (0);
 }
