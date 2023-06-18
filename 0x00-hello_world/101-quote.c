f09112ae3022ee042c8db0d0d5ceb2e9171a4f54
sixthson6 <sudais2408@gmail.com>
2023-06-18 02:44:51 -0700

    fpritf()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,9 @@
 int main(void)
 {
 	FILE *stderr_stream = stderr;
-	fprintf(stderr_stream, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 1;
+
+	char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr_stream, "%s", m);
+	return (1);
 }
