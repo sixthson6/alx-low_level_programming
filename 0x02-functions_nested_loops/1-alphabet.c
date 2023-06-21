2441064daa6288d26ea64a6dd17729b29eefbc95
sixthson6 <sudais2408@gmail.com>
2023-06-21 14:44:57 -0700

    resolve further with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-/**
- * main - Entry point
- * Return: 0 (Success)
- */
-/*int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-*/
 
 /**
  * print_alphabet - print the alphabets using _putcahr
