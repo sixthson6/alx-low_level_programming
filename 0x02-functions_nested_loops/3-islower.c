80ff7b3b936d574fffacc7459f63a8a9e919b50d
sixthson6 <sudais2408@gmail.com>
2023-06-21 15:30:04 -0700

    determine whether or not the char is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _islower - checks whether a char is lowercase
+ * @char c: input
+ * Return: 1 or 0
+ */
+int _islower(char c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
