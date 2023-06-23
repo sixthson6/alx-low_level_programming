a399bbb4356e13daf5dc12701be815499b3467c0
sixthson6 <sudais2408@gmail.com>
2023-06-23 05:52:42 -0700

    print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 to 9
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
