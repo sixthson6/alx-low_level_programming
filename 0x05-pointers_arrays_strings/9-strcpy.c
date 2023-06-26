9b5ba9b551f2c056766f1133c2211bd8d14eda39
sixthson6 <sudais2408@gmail.com>
2023-06-26 13:51:11 -0700

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _strcpy - copy source string to dest
+ * @scr: string to be copied
+ * @dest: copied string
+ * Return: dest (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	char *dest_start = dest;
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest = '\0';
+	return (dest_start);
+}
