c51457f46ed071618f70c68b38efbf1b6c2a0408
sixthson6 <sudais2408@gmail.com>
2023-06-26 10:31:45 -0700

    half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - print second half of string
+ * @str: sting input
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (strlen(str) % 2 == 0)
+		{
+			if (i >= (strlen(str))/2)
+			{
+				_putchar(str[i]);
+			}
+		}
+		else
+		{
+			if (i >= (strlen(str) -1)/2)
+			{
+				_putchar(str[i]);
+			}
+		}
+	}
+	_putchar('\n');
+}
