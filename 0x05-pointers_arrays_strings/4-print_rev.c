79774ead98e06b1efe43b69d6d7af05e64f11465
sixthson6 <sudais2408@gmail.com>
2023-06-26 09:20:39 -0700

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - reverse print
+ * @s: input param
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ void reset_to_98(int *n);
 int _strlen(char *s);
 int _putchar(char c);
 void _puts(char *str);
+void print_rev(char *s);
