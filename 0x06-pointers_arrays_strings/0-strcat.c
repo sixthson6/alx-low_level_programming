27901321eb6c7ae9266ad3fed49d7ce7dfc24484
sixthson6 <sudais2408@gmail.com>
2023-06-30 03:35:22 -0700

    concatenate tow strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+int i, j;
 
-	for (i = src[0]; src[i] < '\0'; i++)
-	{
-		dest[strlen(dest) + i] = src[i];
-	}
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+
+while (src[j] != '\0')
+{
+dest[i + j] = src[j];
+j++;
+}
+dest[i + j] = '\0';
+
+return (dest);
 }
