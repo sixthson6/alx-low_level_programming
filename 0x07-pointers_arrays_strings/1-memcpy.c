bcac1a63ff5827521fe0b66a3e11cd834f5acd95
sixthson6 <sudais2408@gmail.com>
2023-07-03 16:05:02 -0700

    rewrite memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	size_t i;
 
-	for (i = 0; src[i] < '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (i < n)
-		{
 		dest[i] = src[i];
-		}
 	}
 	return (dest);
 }
