059ec249252db8c49be6af503c0fe64e051b8477
sixthson6 <sudais2408@gmail.com>
2023-07-06 17:59:17 -0700

    recursive puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,17 +6,15 @@
  */
 void _puts_recursion(char *s)
 {
-	static int i = 0;
 
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 	else
 	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
 }
