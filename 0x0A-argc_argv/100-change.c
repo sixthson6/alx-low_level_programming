fe01aa632c549c154fbaa3024770f0bf5b186470
sixthson6 <sudais2408@gmail.com>
2023-07-12 17:36:22 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,16 +17,20 @@ int main(int argc, char *argv[])
 	}
 	cents = atoi(argv[1]);
 
-	if (cents < 0 )
+	if (cents < 0)
 	{
 		printf("0\n");
 	}
 	num_coins = 0;
 
-	num_coins += cents / 25; cents %= 25;
-	num_coins += cents / 10; cents %= 10;
-	num_coins += cents / 5; cents %= 5;
-	num_coins += cents / 2; cents %= 2;
+	num_coins += cents / 25;
+	cents %= 25;
+	num_coins += cents / 10;
+	cents %= 10;
+	num_coins += cents / 5;
+	cents %= 5;
+	num_coins += cents / 2;
+	cents %= 2;
 	num_coins += cents;
 
 	printf("%d\n", num_coins);
