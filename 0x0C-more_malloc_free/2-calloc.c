33a04f2869fb869961013e103e876a2524fc9240
sixthson6 <sudais2408@gmail.com>
2023-07-14 12:00:13 -0700

    rewrite calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * _calloc - rewrite calloc
+ * @nmemb: block
+ * @size: bytes to allocate
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+
+	a = 0;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc(nmemb * size);
+	return (a);
+}
