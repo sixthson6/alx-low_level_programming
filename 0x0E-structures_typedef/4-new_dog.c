86608d5e00620496d1aa185c08a55b77e952e47a
sixthson6 <sudais2408@gmail.com>
2023-07-18 03:50:40 -0700

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,36 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
 
 /**
  * new_dog - new struct
@@ -16,8 +45,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 
-	strcpy(new_dog->name, name);
+	_strcpy(new_dog->name, name);
 	new_dog->age = age;
-	strcpy(new_dog->owner, owner);
+	_strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
