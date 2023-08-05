6507730e833a20edbb432178ea7e9a6b703a80d3
sixthson6 <sudais2408@gmai.com>
2023-08-05 16:34:22 -0700

    include guards

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(char c);
 int _isalpha(char c);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
