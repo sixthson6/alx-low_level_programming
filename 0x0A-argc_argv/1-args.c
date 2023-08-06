78764f4ed392240781d10ddb6fb1752a1869eff3
sixthson6 <sudais2408@gmai.com>
2023-08-06 02:02:17 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: agr num
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
