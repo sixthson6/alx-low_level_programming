d25aacf71e36db3efb33271bd5bae6cacb94a67a
sixthson6 <sudais2408@gmai.com>
2023-08-10 14:54:16 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - calloc
+ * @nmemb - blocks
+ * @size - size of blocks
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *res;
+
+	if (nmemb == 0)
+		return (NULL);
+	
+	if (size == 0)
+		return (NULL);
+
+	res = calloc(nmemb, size);
+
+	if (res == NULL)
+		return (NULL);
+	return (res);
+}
