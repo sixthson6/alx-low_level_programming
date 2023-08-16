87af6890a3ca1e18cc9f98790734c1b6b9d173a4
sixthson6 <sudais2408@gmai.com>
2023-08-16 15:38:19 -0700

    cmp func pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - locate index
+ * @array: array
+ * @size: size of array
+ * @cmp: funtion pointer
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
