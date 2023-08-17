b80422d8d41bb01e69c8bfee29e828477a91c7c4
sixthson6 <sudais2408@gmai.com>
2023-08-16 17:53:58 -0700

    print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - print opcodes in hexadecimal
+ * @num_bytes: number of bytes
+ * Return: Nothing
+ */
+
+void print_opcodes(int num_bytes)
+{
+	char *ptr;
+	int i;
+
+	ptr = (char *)print_opcodes;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", ptr[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry
+ * @argc: count
+ * @argv: vector
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	print_opcodes(num_bytes);
+
+	return (0);
+}
