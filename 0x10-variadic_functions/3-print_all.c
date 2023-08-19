db962a0627225e3528d4dee07e30716b772c1aa4
sixthson6 <sudais2408@gmai.com>
2023-08-18 21:58:27 -0700

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,59 +1,108 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
- * print_all - print any type of arg
- * @format: list of args
+ * funct_char_print - print char
+ * @args: arg to print
+ * Return: Nothing
+ */
+
+void funct_char_print(va_list args)
+{
+	int letter;
+
+	letter = (char) va_arg(args, int);
+	printf("%c", letter);
+}
+
+/**
+ * funct_int_print - print int
+ * @args: arg to print
+ * Return: Nothing
+ */
+
+void funct_int_print(va_list args)
+{
+	int num;
+
+	num = va_arg(args, int);
+	printf("%d", num);
+}
+
+/**
+ * funct_float_print - print float
+ * @args: arg to print
+ * Return: Nothing
+ */
+
+void funct_float_print(va_list args)
+{
+	double flt;
+
+	flt = va_arg(args, double);
+	printf("%f", flt);
+}
+
+/**
+ * funct_string_print - print string
+ * @args: arg to print
+ * Return: Nothing
+ */
+
+void funct_string_print(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	
+	if (s == NULL)
+		s = "(nil)";
+	
+	printf("%s", s);
+}
+
+/**
+ * pirnt_all - print all format types c,i,f,s
+ * @format: format specifyers
  * Return: Nothing
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	unsigned int i = 0;
-	char c_arg;
-	int i_arg;
-	double f_arg;
-	char *s_arg;
+	va_list args;
+	int i = 0, j = 0;
+	char *separator;
+	print_funct functions[] = {
+		{"c", funct_char_print},
+		{"i", funct_int_print},
+		{"f", funct_float_print},
+		{"s", funct_string_print}
+	};
 
-	va_start(arg, format);
+	separator = "";
 
-	while (format && format[i])
+	va_start(args, format);
+
+	while (format && (*(format + i)))
 	{
-		if (i > 0)
-			printf(", ");
+		j = 0;
 
-		switch (format[i])
+		while (j < 4 && (*(format + i) != *(functions[j].sym)))
 		{
-			case 'c':
-				c_arg = va_arg(arg, int);
-				printf("%c", c_arg);
-				break;
-			case 'i':
-				i_arg = va_arg(arg, int);
-				printf("%d", i_arg);
-				break;
-			case 'f':
-				f_arg = va_arg(arg, double);
-				printf("%f", f_arg);
-				break;
-			case 's':
-				s_arg = va_arg(arg, char *);
-				if (s_arg == NULL)
-				{
-					printf("(nil)");
-				}
-				else
-				{
-					printf("%s", s_arg);
-				}
-				break;
+			j++;
 		}
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			functions[j]._print(args);
+			separator = ", ";
+		}
+
 		i++;
 	}
-
-	va_end(arg);
-
 	printf("\n");
+
+	va_end(args);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,29 @@
+#include <stdarg.h>
+
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
+/**
+ * struct printer - struct for print_all fucntion
+ * @sym: string holder
+ * @_print: func pointer to func_****_print functions
+ */
+
+typedef struct printer
+{
+	char *sym;
+	void (*_print)(va_list args);
+} print_funct;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void funct_char_print(va_list args);
+void funct_int_print(va_list args);
+void funct_float_print(va_list args);
+void funct_string_print(va_list args);
 void print_all(const char * const format, ...);
+
 
 #endif
