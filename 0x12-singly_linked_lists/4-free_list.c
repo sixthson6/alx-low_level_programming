121c00cccc599a0e5d6f0d457e2406d348b3844d
sixthson6 <sudais2408@gmai.com>
2023-08-24 22:49:56 -0700

    4-free_list.c added

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees memory allocated
+ * @head: memory allocated
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
