280885472014f93d4fec03d0daafb0786ad0aecc
sixthson6 <sudais2408@gmai.com>
2023-08-25 13:57:50 -0700

    constructor functions

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_main - pre main func()
+ * Return: Nothing
+ */
+
+void __attribute__((constructor)) pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
