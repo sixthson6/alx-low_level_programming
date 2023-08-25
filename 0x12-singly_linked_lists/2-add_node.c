4b890d33bb40696c2c9edeece8acfba9063f770c
sixthson6 <sudais2408@gmai.com>
2023-08-24 21:39:52 -0700

    2-add_node.c update

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - append note to linked list
+ * @head: pointer to the current head
+ * @str: value of new head
+ * Return: addr of new element or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = malloc(sizeof(list_t));
+	
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+
+	temp->len = strlen(str);
+	temp->next = *head;
+	
+	*head = temp;
+	
+	return (temp);
+}
