170e281c379036113926d2b5ab69c6f1c3b8e26a
sixthson6 <sudais2408@gmail.com>
2023-08-29 20:08:14 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elements of linked list
+ * @h: head node
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	const listint_t *curr_node = h;
+
+	while (curr_node != NULL)
+	{
+		printf("%d\n", curr_node->n);
+		curr_node = curr_node->next;
+		count++;
+	}
+	return (count);
+}
