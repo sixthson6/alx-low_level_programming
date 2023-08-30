6b0d4e0d73d4f176b2c4fb409ebbf75a314a0e25
sixthson6 <sudais2408@gmail.com>
2023-08-30 14:53:03 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count;
 	listint_t *current = head;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (current != NULL)
 	{
 		if (count == index)
