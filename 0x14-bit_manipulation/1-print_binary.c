556158655a57c5b50771c12eff76d16be7ab7b99
sixthson6 <sudais2408@gmail.com>
2023-08-31 17:07:45 -0700

    1-print_binary.c edit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,8 @@
 
 void print_binary(unsigned long int n)
 {
-	int bit, max_i, i = 0;
+	int bit;
+	unsigned long int max_val;
 
 	if (n == 0)
 	{
@@ -17,13 +18,12 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	max_i = sizeof(unsigned long int) * 8;
+	max_val = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
-	while (n > 0 && i < max_i)
+	while (n > 0 && n != max_val)
 	{
 		bit = n & 1;
 		_putchar(bit + '0');
 		n >>= 1;
-		i++;
 	}
 }
