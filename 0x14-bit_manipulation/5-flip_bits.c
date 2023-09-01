7423c90e8f7472665dcd5462f639b4b784db7c93
sixthson6 <sudais2408@gmail.com>
2023-08-31 19:02:50 -0700

    5-flip_bits.c redo

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,20 +11,17 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int bit1, bit2, count = 0;
+	unsigned long int bits, count = 0;
 
 	if (n == 0 || m == 0)
 		return (0);
 
-	while (n > 0 && m > 0)
-	{
-		bit1 = n & 1;
-		bit2 = m & 1;
+	bits = n ^ m;
 
-		if (bit1 ^ bit2)
-			count++;
-		n >>= 1;
-		m >>= 1;
+	while (bits > 0)
+	{
+		count += bits & 1;
+		bits >>= 1;
 	}
 	return (count);
 }
