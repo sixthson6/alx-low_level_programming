9bf2b6cbd0a9b870407432b08a54023f2d954ca6
sixthson6 <sudais2408@gmail.com>
2023-08-31 17:25:43 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * get_bit - find bit of index in number
+ * @n: number
+ * @index: index
+ *
+ * Return: value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (n == 0)
+		return (0);
+
+	while (n > 0)
+	{
+		if (index == count)
+			return (n & 1);
+
+		n >>= 1;
+		count++;
+	}
+	return (-1);
+}
