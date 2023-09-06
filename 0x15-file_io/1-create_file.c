163d90b416bae2ba27ceb74602d07d7bcb58d11a
sixthson6 <sudais2408@gmail.com>
2023-09-06 01:40:46 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates file if it doesnt already exist
+ * truncates it if it does
+ * @filname: file name
+ * @text_content: text content
+ *
+ * Return: 1 (Success), -1 (Failure)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
