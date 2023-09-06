08ec6ed8c245ab87f4bd56570bbd4830eae23e75
sixthson6 <sudais2408@gmail.com>
2023-09-06 03:38:14 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,42 +17,35 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
-
 	fd_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_file_to == -1)
 		return (-1);
-
 	fd_file_from = open(argv[1], O_RDONLY, 0664);
 	if (fd_file_from == -1)
 		return (-1);
-
 	bytes_file_from = read(fd_file_from, buffer, BUFF_SIZE);
 	if (bytes_file_from == -1)
 	{
 		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
-
 	bytes_file_to = write(fd_file_to, buffer, bytes_file_from);
 	if (bytes_file_to == -1)
 	{
 		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
-
 	if (close(fd_file_from) == -1)
 	{
 		fprintf(stderr, "Error: Can't close fd %d\n", fd_file_from);
-		exit (100);
+		exit(100);
 	}
-
-	if (close(fd_file_to) == - 1)
+	if (close(fd_file_to) == -1)
 	{
 		fprintf(stderr, "Error: Can't close fd %d\n", fd_file_to);
-		exit (100);
+		exit(100);
 	}
-
 	return (0);
 }
