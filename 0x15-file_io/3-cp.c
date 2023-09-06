666f66b516a1f5029931ad5d2868b8f3b9769d2b
sixthson6 <sudais2408@gmail.com>
2023-09-06 04:23:29 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,6 +11,8 @@
 int main(int argc, char *argv[])
 {
 	int fd_file_from, fd_file_to;
+	struct stat stat_buf;
+	mode_t file_perm;
 	char buffer[BUFF_SIZE];
 	ssize_t bytes_file_from, bytes_file_to;
 
@@ -18,10 +20,26 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}	
+	if (stat(argv[2], &stat_buf))
+	{
+		file_perm = stat_buf.st_mode & 0777;
+		fd_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (fd_file_to == -1)
+			exit(99);
 	}
-	fd_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_file_to == -1)
-		return (-1);
+	if (fchmod(fd_file_to, file_perm) == -1)
+	{
+		exit(99);
+	}
+	else
+	{
+		fd_file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+		if (fd_file_to == -1)
+		{
+			exit(99);
+		}
+	}
 	fd_file_from = open(argv[1], O_RDONLY, 0664);
 	if (fd_file_from == -1)
 	{
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,12 +7,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define BUFF_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
