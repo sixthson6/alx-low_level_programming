775bd9a55133e4395f3ce060167841dbfc5d8979
sixthson6 <sudais2408@gmail.com>
2023-09-28 20:11:50 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - print binary representation of decimal num
+ * @n: decimal
+ *
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int bit;
+	unsigned long int max_val;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	max_val = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	while (n > 0 && n != max_val)
+	{
+		bit = n & 1;
+		_putchar(bit + '0');
+		n >>= 1;
+	}
+}
