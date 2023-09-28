7241f46d3bb02040dbad3cc294ad1763c0c196e8
sixthson6 <sudais2408@gmail.com>
2023-09-28 20:45:56 +0000

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - get endianness of a system
+ *
+ * Return: 0(big endian) 1(little endian)
+ */
+
+int get_endianness(void)
+{
+	unsigned int value;
+	unsigned char *byte_ptr;
+
+	value = 1;
+	byte_ptr = (unsigned char *)&value;
+
+	if (*byte_ptr == 1)
+		return (1);
+	else
+		return (0);
+}
