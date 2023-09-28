8e2e96b80dfe2775b6e48776b23ab45165c49d3e
sixthson6 <sudais2408@gmail.com>
2023-09-28 20:36:10 +0000

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit at index to 1
+ * @n: number
+ * @index: index
+ *
+ * Return: 1 (Success)
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = 1UL << index;
+
+	*n |= mask;
+
+	return (1);
+}
