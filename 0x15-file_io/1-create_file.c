2bef55a32323dd29149b1c3bb4b3418861d4482a
sixthson6 <sudais2408@gmail.com>
2023-10-02 23:16:00 +0000

    1-create_file.c added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while(text_content[text_length] != '\0')
+		while (text_content[text_length] != '\0')
 			text_length++;
 		bytes_written = write(fd, text_content, text_length);
 		if (bytes_written == -1)
