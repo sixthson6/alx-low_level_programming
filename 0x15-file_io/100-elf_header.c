c37a47e2bec9aab2c24546333925da8d7380a20f
sixthson6 <sudais2408@gmail.com>
2023-10-05 21:54:54 +0000

    100-elf_header.c added

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <elf.h>
+
+/**
+ * get_elf - determiner elf file type
+ * @ehdr: ptr to struct of ELF
+ *
+ * Return: Nothing
+ */
+void get_elf(Elf64_Ehdr *ehdr)
+{
+	printf("%-35s", "Type:");
+	switch (ehdr->e_type)
+	{
+	case ET_NONE:
+		printf("NONE (Unknown type)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)\n");
+		break;
+	}
+}
+
+
+
+/**
+ * main - entry point for program to get header of ELF file
+ * @argc: arg of count
+ * @argv: arg of array
+ *
+ * Return: 1 on success, error code on failure
+ */
+int main(int ac, char *av[])
+{
+	printf("ac:%d, av:%p\n", ac, (void *)*av);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
