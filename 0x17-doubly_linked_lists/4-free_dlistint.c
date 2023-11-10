169c103d9663508512cc7edcd673e0c1baa9fd4f
sixth_son6 <sudais2408@gmail.com>
2023-11-10 12:53:05 -0800

    4-free_dlistint.c added

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free linked lists
+ * @head: head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	dlistint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
