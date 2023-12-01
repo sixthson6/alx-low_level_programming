46abcb775e225150ad9b95b9e25bfca4af365be6
sixthson6 <sudais2408@gmail.com>
2023-12-01 19:28:51 +0000

    key index relationship

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,8 @@
  * Return: index
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size){
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
 	unsigned long int hash_value, index;
 
 	hash_value = hash_djb2(key);
@@ -17,4 +18,3 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size){
 
 	return (index);
 }
-	
