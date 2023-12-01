0ce98cb0dac3c8b9a165b663cb609e7aa0fdc4be
sixthson6 <sudais2408@gmail.com>
2023-12-01 18:33:06 +0000

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
 	table->size = size;
 	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 
