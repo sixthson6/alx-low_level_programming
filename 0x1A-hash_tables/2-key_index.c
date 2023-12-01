19c7cc7b4a561b957db2720783678c73b098efdd
sixthson6 <sudais2408@gmail.com>
2023-12-01 19:23:47 +0000

    key index relationship

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives the index of a key
+ * in a hash table
+ * @key: key
+ * @size: size of hash table
+ *
+ * Return: index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size){
+	unsigned long int hash_value, index;
+
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
+
+	return (index);
+}
+	
