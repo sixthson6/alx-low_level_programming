62e890e3939671e7d0e3842559eb264c6e0bb484
sixthson6 <sudais2408@gmail.com>
2024-07-15 23:51:13 -0700

    Add binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,56 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * print_array - print array
+ *
+ * Return: array
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+        size_t i;
+
+        printf("Searching array: ");
+        for (i = left; i <= right; i++)
+        {
+                if (i > left)
+                        printf(", ");
+                printf("%d", array[i]);
+        }
+        printf("\n");
+}
+
+/**
+ * binary_search - search algo
+ * @array: search array
+ * @size: size of array
+ * @value: wanted value
+ *
+ * Return: index of wanted value
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, left, right;
+
+	left = 0;
+	right = size - 1;
+	mid = (left + right) / 2;
+
+	while (left <= right)
+	{
+	mid = (left + right) / 2;
+
+	print_array(array, left, right);
+
+	if (value == array[mid])
+		return mid;
+
+	else if (value > array[mid])
+		left = mid + 1;
+	else
+		right = mid - 1;
+	}	
+	return -1;
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,11 @@
+#include <stddef.h>
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* helper functions */
+void print_array(int *array, size_t left, size_t right);
 
 #endif
